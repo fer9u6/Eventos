ae0f43529e415bf0aa48ecee157b06b839575892
Jose Perez Saborio <jose_ps0@hotmail.com>
2017-06-26 00:27:51 -0600

    Manejo de SqlException en caso de no estar conectado al VPN a la hora del Login.

--- Eventos/Login.cs
+++ Eventos/Login.cs
@@ -34,26 +34,33 @@ namespace Eventos
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=10.1.4.119;Initial Catalog=eventos;Integrated Security=False;User ID=B41441;Password=b41441;Connect Timeout=45;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            con.Open();
-            string userid = textBox1.Text;
-            string password = textBox2.Text;
-            SqlCommand cmd = new SqlCommand("select usuario,clave from Usuario where usuario='" + textBox1.Text + "'and clave='" + textBox2.Text + "'", con);
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            if(dt.Rows.Count > 0)
+            try
             {
-                MessageBox.Show("Usuario correcto, bienvenido a la aplicacion");
-                Menu menu = new Menu();
-                menu.Show();
-                this.Hide();            }
-            else
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "Data Source=10.1.4.119;Initial Catalog=eventos;Integrated Security=False;User ID=B41441;Password=b41441;Connect Timeout=45;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                con.Open();
+                string userid = textBox1.Text;
+                string password = textBox2.Text;
+                SqlCommand cmd = new SqlCommand("select usuario,clave from Usuario where usuario='" + textBox1.Text + "'and clave='" + textBox2.Text + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Usuario correcto, bienvenido a la aplicacion");
+                    con.Close();
+                    Menu menu = new Menu();
+                    menu.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Informacion incorrecta, por favor revise el usuario o/y contraseña.");
+                }
+            }catch(SqlException ex)
             {
-                MessageBox.Show("Informacion incorrecta, por favor revise el usuario o/y contraseña.");
+                MessageBox.Show("Conexion a la base de datos no disponible. Asegurese de estar conectado a la red correcta.");
             }
-            con.Close();
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
