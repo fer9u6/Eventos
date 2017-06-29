89b31e0a9d2dd4948606ef0139096dc2ee401c6a
Mao G <maugl17@hotmail.com>
2017-06-28 23:27:13 -0600

    Version 0.9 Proveedor

--- Eventos/Proveedores.cs
+++ Eventos/Proveedores.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
 
 namespace Eventos
 {
@@ -16,9 +20,17 @@ namespace Eventos
         }
         public int agregarProveedor(string cedula, string nombre, string email, string contacto)
         {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=10.1.4.119;Initial Catalog=eventos;Integrated Security=False;User ID=B41441;Password=b41441;Connect Timeout=45;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Proveedor ",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            int numProv = dt.Rows.Count + 1;
             db.actualizarDatos("insert into Persona(Id) values('" + cedula + "')");
             db.actualizarDatos("insert into PersonaJuridica(Id,Correo,Contacto,Nombre) values ('" + cedula + "', '" + email + "', '" + contacto + "','" + nombre + "' )");
-            return db.actualizarDatos("insert into Proveedor (Id) VALUES('" + cedula + "')");
+            return db.actualizarDatos("insert into Proveedor (Id, IdProveedor) VALUES('" + cedula + "', '" + numProv + "')");
         }
     }
 }
